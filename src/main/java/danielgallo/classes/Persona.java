3c81caca0b79c2af60e7480d39591cdaf471f1e8
Dani Gallo <danigallo@Dani.local>
2024-02-10 21:12:58 +0100

    imc

---
+++ src/main/java/danielgallo/classes/Persona.java
@@ -0,0 +1,21 @@
+package danielgallo.classes;
+
+public class Persona {
+    double estatura;
+    double peso;
+
+    public Persona(double estatura, double peso) {
+        this.estatura = estatura;
+        this.peso = peso;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+  
+
+}
